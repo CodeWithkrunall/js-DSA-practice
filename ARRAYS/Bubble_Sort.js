cf867d487a5984c5067932045b5523ccfbc8f84c
CodeWithkrunall <168397686+CodeWithkrunall@users.noreply.github.com>
2026-03-10 12:14:37 +0000

    save

---
+++ ARRAYS/Bubble_Sort.js
@@ -0,0 +1,17 @@
+
+
+
+const bubbleSort = (arr) => {
+    const n = arr.length;
+
+    for(i=0; i<n; i++){
+        for(j=0; j < n-i-1; j++){
+
+            if(arr[j] > arr[j+1]){
+                [arr[j],arr[j+1]] = [arr[j+1], arr[j]];
+           }
+        }
+    }
+    return arr;
+};
+console.log(bubbleSort([ 20, 40,10, 9,2 ]));
